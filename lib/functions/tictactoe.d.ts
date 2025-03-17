35103692803bb84c3fd171a7d247b98bf25ebc23
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2025-03-17 20:47:11 +0300

    SILVA WAVE MD MARCH 2025 17

--- lib/functions/tictactoe.d.ts
+++ lib/functions/tictactoe.d.ts
@@ -1,9 +1,3 @@
-/* Copyright (C) 2025 Codex.
-Licensed under the MIT License;
-you may not use this file except in compliance with the License.
-Codex - Ziyan
-*/
-
 export declare class TicTacToe {
     /* X PlayerName */
     playerX: string;
